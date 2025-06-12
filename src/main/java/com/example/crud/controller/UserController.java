83fbde2851df15478919ad43649e4c140d2870f6
zeenat.zahra <zeenat.zahra@vroozi.com>
2025-06-12 15:56:45 +0500

    Pom File updated

--- src/main/java/com/example/crud/controller/UserController.java
+++ src/main/java/com/example/crud/controller/UserController.java
@@ -1,8 +1,13 @@
-package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.*;
+package com.example.crud.controller;
 
 import java.util.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
